f5e6cd4e486685d48e70578a276ba9cf0b636a25
AndTrejo <Andytrejo52@gmail.com>
2024-02-18 15:08:24 -0600

    Router.js And Vite.config.js and updated routerlink

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -18,7 +18,7 @@ onMounted(async () => {
   <div class="grid flex-grow grid-cols-4 gap-12 m-20">
       <RouterLink v-for="category in categories" 
       :key="category.id"
-      :to="``"
+      :to="`/question/category/${category.id}`"
       class="bg-teal-800 text-center flex h-32 items-center justify-center rounded-lg text-white border-4 border-teal-900 py-4 font-bold uppercase hover:cursor-pointer hover:text-yellow-600 hover:bg-yellow-100 hover:border-yellow-500 transition-colors duration-300"
       >
           {{ category.name }}
